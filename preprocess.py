cc3bca65de9da69f8b1dfd8106c1e911b85d2374
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-02-28 15:18:39 +0330

    add __getitem__ single image read

--- preprocess.py
+++ preprocess.py
@@ -1,87 +1,7 @@
 import torch
 from torch.utils import data
 
-class Dataset(data.Dataset):
-    """
-    Return Dataset class representing our data set
-    """
-    def __int__(self, list_IDs, labels):
-        """
-        Initialize data set as a list of IDs corresponding to each item of data set and labels of each data
-
-        Args:
-            list_IDs: a list of IDs for each data point in data set
-            labels: label of an item in data set with respect to the ID
-        """
-
-        self.labels = labels
-        self.list_IDs = list_IDs
-
-    def __len__(self):
-        """
-        Return the length of data set using list of IDs
-
-        :return: number of samples in data set
-        """
-        return len(self.list_IDs)
-
-    def __getitem__(self, item):
-        """
-        Generate one item of data set. Here we apply our preprocessing things like halftone styles and
-        subtractive color process using CMYK color model etc. (See the paper for operations)
-
-        :param item: index of item in IDs list
-
-        :return: a sample of data
-        """
-        ID = self.list_IDs[item]
-
-        # Code to load data
-        X = None #
-        y = None #
-
-        return X, y
-
-
-# Enable CUDA
-has_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if has_cuda else "cpu")
-# cudnn.benchmark = True
-
-
-# Datasets
-partition = None # IDs
-labels = None # labels
-
-# Parameters
-params = {
-    'batch_size':64,
-    'shuffle': True,
-    'num_workers':4
-}
-max_epochs = 100
-
-# Generators
-training_set = Dataset(partition['train'], labels)
-training_generator = data.DataLoader(training_set, **params)
-
-# Do same for validation
-
-
-# Loop over epochs to train
-for epoch in range(max_epochs):
-    for local_batch, local_label in training_generator:
-        local_batch, local_label = local_batch.to(device), local_label.to(device)
-        # Model
-
-# Validate model
-with torch.set_grad_enabled(False):
-    pass
-
-
-
-
-
+from PIL import Image
 
 import zipfile
 import zlib
@@ -91,58 +11,77 @@ import errno
 import os
 import shutil
 import zipfile
+import pandas as pd
 
 
-
-#%%
-
-def generate_partition(zip_dir):
+class Dataset(data.Dataset):
     """
-    src = open(zip_dir, 'rb')
-    zf = zipfile.ZipFile(src)
-    for idx,m in enumerate(zf.infolist()):
-        if idx != 0:
-            src.read(30)
-            decomp = zlib.decompressobj(-15)
-            out = open('data/'+m.filename, "wb")
-            result = decomp.decompress(src.read(m.compress_size))
-            out.write(result)
-            result = decomp.flush()
-            out.write(result)
-            print(out)
-            out.close()
-        else:
-            pass
-
-    zf.close()
-    src.close()
+    Return Dataset class representing our data set
     """
+    def __int__(self, txt_path, img_dir, transform):
+        """
+        Initialize data set as a list of IDs corresponding to each item of data set and labels of each data
 
-    TARGETDIR = 'small_extract/'
-    with open(zip_dir, "rb") as zipsrc:
-        zfile = zipfile.ZipFile(zipsrc)
-        for member in zfile.infolist():
-            target_path = os.path.join(TARGETDIR, member.filename)
-            if target_path.endswith('/'):  # folder entry, create
-                try:
-                    os.makedirs(target_path)
-                except:
-                    raise
+        Args:
+            :param img_dir: path to the main tar file of all of images
+            :param txt_path: a text file containing names of all of images line by line
+            :param transform: apply some transforms like cropping, rotating, etc on input image
 
-            with open(target_path, 'wb') as outfile, zfile.open(member) as infile:
-                shutil.copyfileobj(infile, outfile)
-                print(infile)
-                print(outfile)
+            :return a 3-member tuple containing input image (y_descreen) as ground truth, input image X as halftone image
+                    and edge-map (y_edge) of ground truth image to feed into the network.
+        """
+
+        df = pd.read_csv(txt_path, sep=' ', index_col=0)
+        self.img_names = df.index.values
+        self.txt_path = txt_path
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        """
+        Return the length of data set using list of IDs
+
+        :return: number of samples in data set
+        """
+        return len(self.img_names)
+
+    def __getitem__(self, index):
+        """
+        Generate one item of data set. Here we apply our preprocessing things like halftone styles and
+        subtractive color process using CMYK color model, generating edge-maps, etc.
+
+        :param index: index of item in IDs list
+
+        :return: a sample of data
+        """
+
+        y_descreen = Image.open(os.path.join(self.img_dir), self.img_names[index])
+        if self.transform is not None:
+            y_descreen = self.transform(y_descreen)
+
+        # TODO add halftone process here
+        X = None
+
+        # TODO add canny edge detector process here
+        y_edge = None
+
+        return X, y
 
 
-generate_partition('data/small.zip')
+import os
+import tarfile
+
+# %%
+# create sample tar file to test other modules over it.
+import tarfile
+with tarfile.open('data.tar', 'w') as tar:
+    tar.add('data/')
+tar.close()
 
 
-import ctypes, sys
-
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+with tarfile.open('data.tar') as tf:
+    for tarinfo in tf:
+        if os.path.splitext(tarinfo.name)[0] == 'data/Places365_val_00000003':
+            image = tf.extractfile(tarinfo)
+            image = image.read()
+            image = Image.open(io.BytesIO(image))
